676c7f7b880fed8629395e83c5a53c0d422f3dbd
nvanlangen <59780892+nvanlangen@users.noreply.github.com>
2020-05-05 23:40:36 -0400

    added changes for flask api

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
 from flask import Flask, jsonify
+
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 app = Flask(__name__)
 
@@ -17,20 +28,112 @@ def welcome():
 @app.route("/api/v1.0/precipitation")
 def precipitation():
     """Return the precipitation data as json"""
+    session = Session(engine)
+    # Calculate the date 1 year ago from the last data point in the database
+    max_date = session.query(func.max(Measurement.date)).first()
+    min_date = str(int(str(max_date[0])[0:4])-1) + str(max_date[0])[4:10]
+
+    # Perform a query to retrieve the data and precipitation scores
+    precip_results = session.query(Measurement.date,func.avg(Measurement.prcp).label("prcp")).\
+        filter(Measurement.date > min_date ).\
+        group_by(Measurement.date).\
+        order_by(Measurement.date).all()
+
+    session.close()
+    precipitation_data = []
+    for result in precip_results:
+        precip_dict = {}
+        precip_dict["date"] = result.date
+        precip_dict["prcp"] = result.prcp
+        precipitation_data.append(precip_dict)
 
     return jsonify(precipitation_data)
 
 @app.route("/api/v1.0/stations")
 def stations():
     """Return the station data as json"""
+    session = Session(engine)
+    station_results = session.query(Station.station,Station.name,Station.latitude,Station.longitude,Station.elevation)
+
+    session.close()
+    station_data = []
+    for result in station_results:
+        station_dict = {}
+        station_dict["station"] = result.station
+        station_dict["name"] = result.name
+        station_dict["latitude"] = result.latitude
+        station_dict["longitude"] = result.longitude
+        station_dict["elevation"] = result.elevation
+        station_data.append(station_dict)
 
     return jsonify(station_data)
 
 @app.route("/api/v1.0/tobs")
 def tobs():
     """Return the temperature data as json"""
+    session = Session(engine)
+    station_activity=session.query(Measurement.station,func.count(Measurement.station)).\
+    group_by(Measurement.station).\
+    order_by(func.count(Measurement.station).desc()).first()
 
+    most_active_station = station_activity.station
+
+    max_date = session.query(func.max(Measurement.date)).first()
+    min_date = str(int(str(max_date[0])[0:4])-1) + str(max_date[0])[4:10]
+
+    tobs_results = session.query(Measurement.date,Measurement.tobs).\
+        filter(Measurement.station == most_active_station).\
+        filter(Measurement.date > min_date).\
+        order_by(Measurement.date)
+
+    session.close()
+    tobs_data = []
+    for result in tobs_results:
+        tobs_dict = {}
+        tobs_dict["date"] = result.date
+        tobs_dict["tobs"] = result.tobs
+        tobs_data.append(tobs_dict)
+        
     return jsonify(tobs_data)
+
+@app.route("/api/v1.0/<start>")
+def start_date(start):
+    session = Session(engine)
+    temp_results = session.query(Measurement.date,func.min(Measurement.tobs),func.max(Measurement.tobs),func.avg(Measurement.tobs)).\
+        filter(Measurement.date >= start).\
+        group_by(Measurement.date).\
+        order_by(Measurement.date).all()
+
+    session.close()
+    temp_stats = []
+    for result in temp_results:
+        temp_dict = {}
+        temp_dict["date"] = result[0]
+        temp_dict["TMIN"] = result[1]
+        temp_dict["TAVG"] = result[3]
+        temp_dict["TMAX"] = result[2]
+        temp_stats.append(temp_dict)
+    return jsonify(temp_stats)
+
+@app.route("/api/v1.0/<start>/<end>")
+def start_end_date(start,end):
+    session = Session(engine)
+    temp_results = session.query(Measurement.date,func.min(Measurement.tobs),func.max(Measurement.tobs),func.avg(Measurement.tobs)).\
+        filter(Measurement.date >= start).\
+        filter(Measurement.date <= end).\
+        group_by(Measurement.date).\
+        order_by(Measurement.date).all()
+
+    session.close()
+    temp_stats = []
+    for result in temp_results:
+        temp_dict = {}
+        temp_dict["date"] = result[0]
+        temp_dict["TMIN"] = result[1]
+        temp_dict["TAVG"] = result[3]
+        temp_dict["TMAX"] = result[2]
+        temp_stats.append(temp_dict)
+    return jsonify(temp_stats)
 
 if __name__ == "__main__":
     app.run(debug=True)
